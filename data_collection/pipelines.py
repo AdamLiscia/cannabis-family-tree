c15751f0408eb78462052d5c501d8ac241c06119
Michael Wheeler <wheeler.m@husky.neu.edu>
2018-11-11 15:13:26 -0500

    Feat: Added a pipeline to Scrapy to do basic cleaning

--- data_collection/pipelines.py
+++ data_collection/pipelines.py
@@ -4,8 +4,18 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from data_collection.spiders.leafly import ScrapeLeafly
 
 
-class DataCollectionPipeline(object):
+class StrainParentsPipeline(object):
+    """
+    Processes the CannabisStrain items coming out of the spiders. Cleans the fields that are length-one lists and
+    converts relative URLs to absolute ones.
+    """
     def process_item(self, item, spider):
+        if spider.__class__ == ScrapeLeafly:
+            item["name"] = item["name"][0]
+            item["url"] = item["url"][0]
+            item["description"] = item["description"][0]
+            item["parent_strains"] = [f'https://www.leafly.com{strain_url}' for strain_url in item["parent_strains"]]
         return item
